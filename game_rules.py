a0d2810446175bf0dfd0f6ada26abea7743bb5e2
Sarra <martins.yo@gmail.com>
2020-12-18 19:10:17 +0100

    Can check suggestions

--- game_rules.py
+++ game_rules.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from uuid import uuid4
 
 
 @dataclass
@@ -11,25 +12,59 @@ class MurderEvent:
 from enum import Enum
 
 
-class PlayerStatus(Enum):
-    WON = "Won"
-    LOST = "Lost"
-    PLAYING = "Playing"
+class AccusationStatus(Enum):
+    CORRECT = "correct"
+    WRONG = "wrong"
 
 
 @dataclass
-class Player:
-    def __init__(self, room=None, status=PlayerStatus.PLAYING):
+class UnruledPlayer:
+    """This is basic suggestion/accusation behaviour as in last page of rules.
+
+    It does not check pre-conditions of pawns position, whose turn ist, etc. Its very raw and unruled."""
+
+    def __init__(self, room=None, cards=[]):
+        self.cards = set(cards)
+        self.cards_to_check = {}
+        self.last_card_refuted = None
         self.room = room
-        self.status = status
+        self.__hash = uuid4().int
 
     def accuse(self, room, suspect, weapon, casefile):
         accusation = MurderEvent(room, weapon, suspect)
         if accusation == casefile:
-            self.status = PlayerStatus.WON
+            self.status = AccusationStatus.CORRECT
         else:
-            self.status = PlayerStatus.LOST
+            self.status = AccusationStatus.WRONG
+
+    def receive_cards(self, cards):
+        self.cards.update(cards)
 
     def enter_room(self, room):
+        """This sets the room for suggestions"""
         self.room = room
         return self
+
+    def suggest(self, suspect, weapon):
+        """Player suggesions can only relate to the room he is in"""
+        self.suggestion = MurderEvent(self.room, suspect, weapon)
+
+    def check_suggestion(self, other):
+        suggestion_as_set = set(
+            [self.suggestion.room, self.suggestion.suspect, self.suggestion.weapon]
+        )
+        other._receive_check_suggestion(self, suggestion_as_set)
+
+    def _receive_check_suggestion(self, other, suggestion):
+        self.cards_to_check[other] = suggestion.intersection(self.cards)
+
+    def confirm_check_suggestion(self, other, card=None):
+        if len(self.cards_to_check[other]) != 0:
+            try:
+                self.cards_to_check[other].remove(card)
+                other.last_card_refuted = card
+            except KeyError as e:
+                raise Exception("Tried to refute an irrefutable card")
+
+    def __hash__(self):
+        return self.__hash
--- test_gamerules.py
+++ test_gamerules.py
@@ -1,4 +1,4 @@
-from game_rules import MurderEvent, PlayerStatus, Player
+from game_rules import MurderEvent, AccusationStatus, UnruledPlayer
 
 
 def test_accusation():
@@ -6,12 +6,57 @@ def test_accusation():
 
     casefile = MurderEvent(room="Kitchen", suspect="Rev Green", weapon="Candlestick")
 
-    p1 = Player()
+    p1 = UnruledPlayer()
     p1.accuse("Lounge", "Colonel Mustard", "Rope", casefile)
 
-    assert p1.status == PlayerStatus.LOST
+    assert p1.status == AccusationStatus.WRONG
 
-    p2 = Player()
-    p2.enter_room("Hall").accuse("Kitchen", "Rev Green", "Candlestick", casefile)
+    p2 = UnruledPlayer()
+    p2.accuse("Kitchen", "Rev Green", "Candlestick", casefile)
 
-    assert p2.status == PlayerStatus.WON
+    assert p2.status == AccusationStatus.CORRECT
+
+
+def test_suggestion():
+    """Tests the suggestion mechanism
+
+    Each player can see the last refuted card
+
+    """
+
+    p1, p2, p3, p4 = (
+        UnruledPlayer(room="Lounge"),
+        UnruledPlayer(),
+        UnruledPlayer(),
+        UnruledPlayer(),
+    )
+
+    p2.receive_cards(["Kitchen", "Colonel Mustard", "Dagger"])
+    p3.receive_cards(["Leisure room", "Mr. Black", "Knife"])
+    p4.receive_cards(["Lounge", "Rev Green", "Knife"])
+
+    p1.suggest("Rev Green", "Candlestick")
+
+    p1.check_suggestion(p2)
+    p2.confirm_check_suggestion(p1)
+
+    p1.check_suggestion(p3)
+    p3.confirm_check_suggestion(p1)
+
+    p1.check_suggestion(p4)
+    p4.confirm_check_suggestion(p1, "Rev Green")
+    assert p1.last_card_refuted == "Rev Green"
+
+    p1.enter_room("Kitchen").suggest("Colonel Mustard", "Dagger")
+    p1.check_suggestion(p2)
+    p2.confirm_check_suggestion(p1, "Dagger")
+    assert p1.last_card_refuted == "Dagger"
+
+    p1.enter_room("Bathroom").suggest("Colonel Mustard", "Dagger")
+    p3.enter_room("Kitchen").suggest("Rev Green", "Pipe")
+    p3.check_suggestion(p2)
+    p1.check_suggestion(p2)
+    p2.confirm_check_suggestion(p1, "Dagger")
+    p2.confirm_check_suggestion(p3, "Kitchen")
+    assert p1.last_card_refuted == "Dagger"
+    assert p3.last_card_refuted == "Kitchen"
